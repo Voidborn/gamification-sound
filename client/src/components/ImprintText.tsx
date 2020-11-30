794f9e58eae02ffd057a0b061e801b403ec46beb
Vladislav Hnatovskiy <s9vlhnat@stud.uni-saarland.de>
2020-11-30 12:45:14 +0100

    Imprint

--- client/src/components/ImprintText.tsx
+++ client/src/components/ImprintText.tsx
@@ -3,14 +3,16 @@ import React from "react";
 const ImprintText = () => {
     return (
         <div style={{maxHeight:"60vh", overflow:"auto", paddingRight:"30px", textAlign:"justify"}}>
-            Vladislav Hnatovskiy<br/>
 
             <h5>E-Mail Contacts</h5>
+            Vladislav Hnatovskiy:<br/>
             <a href="s9vlhnat@stud.uni-saarland.de">s9vlhnat@stud.uni-saarland.de</a>
-            <br/>
+            <br />
+            Maximilian Altmeyer:<br/>
             <a href="maximilian.altmeyer@dfki.de">maximilian.altmeyer@dfki.de</a>
 
             <h5>Postal Address</h5>
+            Vladislav Hnatovskiy<br/>
             Westpreußenring 21<br />
             66121 Saarbrücken<br />
             Germany<br />
